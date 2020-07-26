19469c847c4e244bff8bb7ae8944745bb6dc5b96
Italicz <james.angier@griffithuni.edu.au>
2020-07-26 14:13:16 +1000

    Created routes and login form

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express'); //Used for routing
 const { allowedNodeEnvironmentFlags } = require('process');
 var app = express();
 var http = require('http').Server(app); //Used to provite the HTTP functionality
+var path = require('path');
+require('./routes/accountroute.js')(app,path);
+require('./routes/loginroute.js')(app,path);
 
 app.use(express.static(__dirname +'/www'));
 
@@ -11,7 +14,6 @@ let server = http.listen(3000, function() {
     console.log("My First Nodejs Server!");
     console.log("Server listening on: "+ host + " port:" + port);
 })
-
 
 app.get('/test', function (req, res) {
     res.sendFile(__dirname + '/www/test.html');
